aa84c4bc15d6caa6595985c6a7e59e416fde9aea
iyengaraditya <56295215+iyengaraditya@users.noreply.github.com>
2020-04-09 00:09:48 +0530

    Script that performs the tidying tasks

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-##Downloads the file and unzips it
+##Downloads the zip file and unzips it
 if(!file.exists("./data")){dir.create("./data")}
 #Here are the data for the project:
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
